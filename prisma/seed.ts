1ff7a075ad954f0781847eceeb70c277618d337d
Sinh <sinh117801@gmail.com>
2022-06-16 09:31:43 +0700

    bump dependencies and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { hashSync } from 'bcryptjs';
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ['query'],
+});
 
 async function main() {
   // Init User
